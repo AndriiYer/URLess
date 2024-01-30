cd5091ce722aa247d4350cb142c92e63c66cdca7
Andriyerm <iermolchev@gmail.com>
2024-01-30 14:17:04 +0200

    added test data structures

---
+++ AndriYer.URLess.Data/RedisDictionary.cs
@@ -0,0 +1,104 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections;
+
+namespace AndriYer.URLess.Data
+{
+    public class RedisDictionary : IDictionary<string, string?>
+    {
+        private readonly IDatabase _db;
+        public RedisDictionary(string configuration)
+        {
+            var redis = ConnectionMultiplexer.Connect(configuration);
+            _db = redis.GetDatabase();
+        }
+        
+        public ICollection<string> Keys => throw new NotImplementedException();
+        
+        public ICollection<string?> Values => throw new NotImplementedException();
+        
+        public int Count => throw new NotImplementedException();
+        
+        public bool IsReadOnly => false;
+        
+        public string? this[string key]
+        {
+            get => _db.StringGet(key);
+            set => _db.StringSet(key, value);
+        }
+        
+        public void Add(string key, string value)
+        {
+            _db.StringSet(key, value);
+        }
+        
+        public void Add(KeyValuePair<string, string> item)
+        {
+            _db.StringSet(item.Key, item.Value);
+        }
+        
+        public void Clear()
+        {
+            // Not supported in Redis
+            throw new NotSupportedException();
+        }
+        
+        public bool Contains(KeyValuePair<string, string> item)
+        {
+            var value = _db.StringGet(item.Key);
+            return value.HasValue && value == item.Value;
+        }
+        
+        public bool ContainsKey(string key)
+        {
+            return _db.KeyExists(key);
+        }
+        
+        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        {
+            // Not supported in Redis
+            throw new NotSupportedException();
+        }
+        
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            // Not supported in Redis
+            throw new NotSupportedException();
+        }
+        
+        public bool Remove(string key)
+        {
+            return _db.KeyDelete(key);
+        }
+        
+        public bool Remove(KeyValuePair<string, string> item)
+        {
+            if (Contains(item))
+            {
+                return _db.KeyDelete(item.Key);
+            }
+            return false;
+        }
+        
+        public bool TryGetValue(string key, out string value)
+        {
+            var redisValue = _db.StringGet(key);
+            if (redisValue.HasValue)
+            {
+                value = redisValue;
+                return true;
+            }
+            else
+            {
+                value = null;
+                return false;
+            }
+        }
+        
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            // Not supported in Redis
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ AndriYer.URLess.WebApi.Tests/Data/TestUrlRepository.cs
@@ -0,0 +1,18 @@
+﻿using AndriYer.URLess.Data.Contracts;
+using AndriYer.URLess.Domain.Models;
+using System.Collections.Concurrent;
+
+namespace AndriYer.URLess.WebApi.Tests.Data
+{
+    public class TestUrlRepository(TestDataBase database) : IUrlRepository
+    {
+        public Task<Url> GetByShortUrl(string shortUrl, CancellationToken cancellationToken = default) =>
+            database.GetValueOrDefault(shortUrl, cancellationToken);
+
+        public Task Add(Url url, CancellationToken cancellationToken = default) => 
+            database.Add(url, cancellationToken);
+
+        public Task<bool> Contains(string shortUrl, CancellationToken cancellationToken = default) =>
+            database.Contains(shortUrl, cancellationToken);
+    }
+}
